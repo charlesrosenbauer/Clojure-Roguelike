aac46b6861849fc640292c0aa7bac94f224fbc01
Charles Rosenbauer <charles.rosenbauer@gmail.com>
2018-09-18 23:39:25 +0000

    Some simple functions for the beginnings of a world map and renderer.

--- src/roguelike/core.clj
+++ src/roguelike/core.clj
@@ -5,3 +5,42 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+
+
+
+
+
+
+
+
+
+(defn fillBlock
+  "Creates a block, fills it with a specified tile. I should probably optimize this later."
+  [tile]
+  [[tile tile tile tile]
+   [tile tile tile tile]
+   [tile tile tile tile]
+   [tile tile tile tile]])
+
+
+
+
+
+
+
+
+
+
+(defn showableworld
+  "Filters blocks in based on whether or not they are viewable."
+  [world x y rx ry]
+  (let
+    [lox (- x (/ rx 2))
+     hix (+ x (/ rx 2))
+     loy (- y (/ ry 2))
+     hiy (+ y (/ ry 2)) ]
+     (filter (fn [bx by block]
+                            (and
+                             (< bx hix) (> bx lox)
+                             (< by hiy) (> by loy))) world)))
