fd0ff9e41b02d21bdb912c7ef4a40e6d78d027e7
Charles Rosenbauer <charles.rosenbauer@gmail.com>
2018-09-21 14:14:56 +0000

    Added some initial code for creating and printing a world

--- src/roguelike/core.clj
+++ src/roguelike/core.clj
@@ -11,8 +11,9 @@
 
 
 
-(defstruct Block :X :Y :Tiles)
-(defstruct World :Blocks)
+(defstruct Block  :X :Y :Tiles)
+(defstruct World  :Blocks :Entities)
+(defstruct Entity :X :Y :Tile :Id)
 
 
 
@@ -43,7 +44,7 @@
         row (take 8 (cycle [realtile]))
 	      mat (take 8 (cycle [row     ]))]
 
-        (struct Block x y mat)))
+        (struct Block (int x) (int y) mat)))
 
 
 
@@ -66,7 +67,50 @@
                 (let [bx (:X block)
                       by (:Y block)]
                      (and (< bx hix) (> bx lox)
-                          (< by hiy) (> by loy))) world))))
+                          (< by hiy) (> by loy))) (:Blocks world)))))
+
+
+
+
+
+
+
+
+
+
+(defn tileAtCoord
+  [world x y]
+  (let [x_ (int (/ x 8))
+        y_ (int (/ y 8))
+        xi (mod (int x) 8)
+        yi (mod (int y) 8)
+        block (get (:Blocks world) [x_ y_])]
+    (if (= nil block)
+      " "
+      (get (get (:Tiles block) yi) xi))))
+
+
+
+
+
+
+
+
+
+
+(defn showWorld
+  "Returns a string to draw the world."
+  [world x y rx ry]
+  (let
+    [ents   (:Entities world)
+     show   (showableworld world)
+     rx_    (* 8 rx)
+     ry_    (* 8 ry)
+     row    (range (- x rx_) (+ x rx_))
+     col    (range (- y ry_) (+ y ry_))
+     coords (map (fn [i] (into [] (map #(vector i %) row))) col)
+     tiles  (map #(map (fn [[a b]] (tileAtCoord world a b)) %) coords)]
+  (apply str (map #(map (cons "\n" (fn [[a b]] (tileAtCoord world a b)) %)) coords))))
 
 
 
@@ -95,7 +139,7 @@
   [topdoor botdoor lftdoor rgtdoor x y]
 
   (let [coords (map (fn [i] (into [] (map #(vector i %) (range 0 8)))) (range 0 8))]
-       (struct Block x y
+       (struct Block (int x) (int y)
          (map #(into [] (map (fn [[a b]]
                 (cond
                   (and (= a 0) (= b topdoor)) "."
@@ -104,6 +148,19 @@
                   (and (= b 7) (= a rgtdoor)) "."
                   (or  (= a 0) (= a 7) (= b 0) (= b 7)) "#"
                   :else ".")) %)) coords))))
+
+
+
+
+
+
+
+
+
+
+(defn addBlockToWorld
+  [world block]
+  (assoc world block [(:X block) (:Y block)]))
 
 
 
